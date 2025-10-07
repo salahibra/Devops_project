19cb4c0c091193c7cfbdc710876b01c010ef2e22
ibrahim salah <ibrahimsalahzefri75@gmail.com>
2025-10-07 21:28:48 +0200

    connecting angular project to firebase to use firebase products

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,14 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), provideClientHydration(withEventReplay()), provideFirebaseApp(() => initializeApp({ projectId: "earthvibes-firebase-project", appId: "1:580894066907:web:4054eb4c0e306aa13324fe", storageBucket: "earthvibes-firebase-project.firebasestorage.app", apiKey: "AIzaSyCoK8ZUPg1aQF6AYA5-71Q8h8qV8KMjmx8", authDomain: "earthvibes-firebase-project.firebaseapp.com", messagingSenderId: "580894066907"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Firebase_Project');
+  protected readonly title = signal('firebase-project');
 }
